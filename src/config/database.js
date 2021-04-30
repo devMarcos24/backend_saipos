3646959954fb69dd7006403c3a00e37b580a310c
Marcos Taylor <dev.marcos1995@gmail.com>
2021-04-30 00:17:48 -0300

    fix: connection with database

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,29 @@
 const _env = require("dotenv").config();
 
 module.exports = {
-  // host: process.env.DATABASE_HOST,
-  // username: process.env.DATABASE_USERNAME,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_NAME,
-  // dialect: "postgres",
-  // port: process.env.DATABASE_PORT,
-  // ssl: true,
-  // define: {
-  //   timestamps: true,
-  // },
-  host: "localhost",
-  username: "postgres",
-  password: "docker",
-  database: "postgres",
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
   dialect: "postgres",
-  port: 5432,
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     timestamps: false,
   },
+  // host: "localhost",
+  // username: "postgres",
+  // password: "docker",
+  // database: "postgres",
+  // dialect: "postgres",
+  // port: 5432,
+  // define: {
+  //   timestamps: false,
+  // },
 };
--- src/shared/database/index.js
+++ src/shared/database/index.js
@@ -11,8 +11,30 @@ class Database {
   }
 
   init() {
-    const sequelize = new Sequelize(databaseConfig); // Example for postgres
-    // const sequelize = new Sequelize(databaseConfig);
+    const sequelize = new Sequelize({
+      host: process.env.DATABASE_HOST,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT,
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      define: {
+        timestamps: false,
+      },
+    });
+
+    sequelize.sync;
+    sequelize
+      .authenticate()
+      .then((res) => console.log("database conected"))
+      .catch((err) => console.log(err));
 
     models
       .map((model) => model.init(sequelize))
