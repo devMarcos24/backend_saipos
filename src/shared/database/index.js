744f4673ae180d1c1ec1bb7a4848a4162a178fd8
Marcos Taylor <dev.marcos1995@gmail.com>
2021-04-29 23:52:40 -0300

    Keep calm and commit

--- src/shared/database/index.js
+++ src/shared/database/index.js
@@ -1,13 +1,7 @@
-import pg from "pg";
 import { Sequelize } from "sequelize";
 
 import databaseConfig from "../../config/database";
 import todo from "../Models/todos";
-var client = new pg.Client(
-  "postgres://ouiqcmfdjgcmtb:877291d9e2c3f58aaed0fc4bc8cefea7d8dca86f9c49a25d2b7fe234cbc8d593@ec2-18-214-140-149.compute-1.amazonaws.com:5432/d182ch888mejf7"
-);
-
-client.connect();
 
 const models = [todo];
 
